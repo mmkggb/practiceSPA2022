2ff543de21dbaf12c296eb380e4ba61a6d56213e
Michael Kelly <martisemk@gmail.com>
2022-03-02 21:34:58 -0600

    First Commit, Fully functional SPA

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+import html from "html-literal";
+// let year = new Date().getFullYear;
+export default () => html`
+  <footer>
+    &copy; 2022 <a href="https://savvycoders.com/">Savvy Coders</a>
+  </footer>
+`;
+
+// ${year}
---
+++ store/Order.js
@@ -0,0 +1,4 @@
+export default {
+  header: "Place a pizza order",
+  view: "Order"
+};
